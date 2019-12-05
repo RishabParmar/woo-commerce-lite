41d469209e93b0055389e0b5685c71686a742f6a
Rishab Parmar <parmar.rishab@gmail.com>
2019-12-05 17:07:12 +0530

    Removed credentials.js

--- credentials.js
+++
@@ -1,6 +0,0 @@
-cust_credentials = {
-    consumer_key: 'ck_eb3efab406f266eace3f317bd996d4df3932ad78', 
-    consumer_secret:'cs_370d1cd474c60b523f2d03956ba82ba8b62e33bc',
-};
-
-module.exports = cust_credentials;
